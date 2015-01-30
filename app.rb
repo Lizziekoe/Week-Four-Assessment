69aac472a2900f4009cb81f8fc9fb84838cfb43b
Lizzie Koehler <lizziefkoehler@gmail.com>
2015-01-30 10:02:48 -0800

    initiate sinatra using app.rb file

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require("bundler/setup")
 require("pry")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @venues = Venue.all
+  erb(:index)
+end
+
+post('/venues') do
+  venue = params.fetch("venue")
+end
