f5e2da403077f36c28c6f17fb42f1cd5287a44ca
Lizzie Koehler <lizziefkoehler@gmail.com>
2015-01-30 10:22:45 -0800

    add a bands page in sinatra

--- app.rb
+++ app.rb
@@ -9,5 +9,21 @@ get('/') do
 end
 
 post('/venues') do
-  venue = params.fetch("venue")
+  @venues = Venue.all
+  venue_name = params.fetch("venue")
+  venue = Venue.create(:name => venue_name)
+  erb(:index)
+end
+
+get('/venues/:id') do
+  @venue = Venue.find(params.fetch("id").to_i)
+  @bands = Band.all
+  erb(:add_bands)
+end
+
+post('/bands') do
+  @bands = Band.all
+  band_name = params.fetch("band")
+  band = Band.create(:name => band_name)
+  erb(:add_bands)
 end
